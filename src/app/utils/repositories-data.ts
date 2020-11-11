a0e1c082a33fced7a5b384c9bd6ac8760fb5b444
AntonVoronezh <pstut@mail.ru>
2020-11-11 10:00:48 +0300

    RepositoriesData

---
+++ src/app/utils/repositories-data.ts
@@ -0,0 +1,20 @@
+import { cloneDeep } from 'lodash';
+
+import { IGithubRepository, IGithubRepositoryItem, IGithubRepositoryList } from '../models/github-repositorie';
+
+export class RepositoriesData {
+  private data: IGithubRepository;
+
+  log(): void {
+    console.log('RepositoriesData', this.data);
+  }
+
+
+
+
+
+
+
+
+
+}
