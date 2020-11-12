5fd1ed70e8bd885d30002a927adf88b599a93f0e
AntonVoronezh <pstut@mail.ru>
2020-11-12 16:05:32 +0300

    InsuranceLocalstorageService

---
+++ src/app/services/localstorage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { IGithubRepositoryList } from '../models/github-repositorie';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InsuranceLocalstorageService {
+  private readonly GITHUB_REPOSITORIES = 'github_repositories';
+
+
+
+
+
+
+
+
+}
